6b57d142474d1e2fa03eef0bdbadc90451867024
Victor Rodrigues <vitumail@gmail.com>
2016-07-21 00:20:19 +0200

    Fix edge build with new Elixir

--- lib/red/edge.ex
+++ lib/red/edge.ex
@@ -2,15 +2,20 @@ defmodule Red.Edge do
   defstruct rel: nil, target: nil
 
   def ops(%Red.Edge{} = edge, :add) do
-    [s, e] =
+    [origin, target] =
       [edge.rel.node, edge.target]
       |> Enum.map(&Red.key &1)
-
-    if edge.rel.direction == :out, do: [s, e] = [e, s]
+      |> conform_to_direction(edge)
 
     [
-      ["ZADD", "#{s}:#{edge.rel.name}:in",  0, e],
-      ["ZADD", "#{e}:#{edge.rel.name}:out", 0, s]
+      ["ZADD", "#{origin}:#{edge.rel.name}:in",  0, target],
+      ["ZADD", "#{target}:#{edge.rel.name}:out", 0, origin]
     ]
   end
+
+  defp conform_to_direction([origin, target], %{rel: %{direction: :out}}) do
+    [target, origin]
+  end
+
+  defp conform_to_direction([origin, target], _), do: [origin, target]
 end
